19278f3a066ebec6b1dcece154595e68e8e1e1ff
Aleex'x <aorostegui2@gmail.com>
2024-03-08 17:59:02 -0500

    Implementacion de la clase cita Enfermero

--- src/main/java/org/ptech/java/citas/entities/CitaEnfermero.java
+++ src/main/java/org/ptech/java/citas/entities/CitaEnfermero.java
@@ -1,7 +1,61 @@
 package org.ptech.java.citas.entities;
 
+import java.time.LocalDateTime;
 
-public class CitaEnfermero {
+import org.ptech.java.citas.entities.enums.EstadoCita;
+import org.ptech.java.citas.interfaces.IAgendable;
+
+public class CitaEnfermero  extends Cita implements IAgendable{
+
+    Enfermero enfermero;
+    EstadoCita estado;
+
+    
+
+    public CitaEnfermero(int id, LocalDateTime fecha, Consultorio consultorio, Paciente paciente, Enfermero enfermero,
+            EstadoCita estado) {
+        super(id, fecha, consultorio, paciente);
+        this.enfermero = enfermero;
+        this.estado = estado;
+    }
+
+    public Enfermero getEnfermero() {
+        return enfermero;
+    }
+
+    public void setEnfermero(Enfermero enfermero) {
+        this.enfermero = enfermero;
+    }
+
+    public EstadoCita getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoCita estado) {
+        this.estado = estado;
+    }
+
+
+
+    @Override
+    public boolean agenarCita(LocalDateTime fecha, Consultorio c) {
+        this.setFecha(fecha);
+        this.setConsultorio(c);
+        return true;
+    }
+
+    @Override
+    public boolean reAgendarCita(LocalDateTime nuevaFecha, Consultorio nuevoc) {
+        this.setFecha(nuevaFecha);
+        this.setConsultorio(nuevoc);
+        return true;
+    }
+
+    @Override
+    public boolean cancelarCita() {
+        this.estado = EstadoCita.CANCELADA;
+        return true;
+    }
 
 
 }
